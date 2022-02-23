89cd392f461b00ba0c5b71f82c7dce5294540e77
yuribertoli <yuribertoli@yahoo.it>
2022-02-23 18:41:07 +0100

    header

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <MyHeader/>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    MyHeader
+  }
+}
+</script>
+
+<style lang="scss">
+ @import "./assets/style/general.scss";
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+      <img src="../assets/img/spotify-logo.png" alt="Spotify Logo">
+  </header>
+</template>
+
+<script>
+export default {
+    name: "MyHeader"
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/style/variables.scss";
+
+    header {
+        height: 50px;
+        background-color: $color1;
+
+        img {
+            height: 80%;
+            width: auto;
+            padding: 10px 0 0 20px;
+        }
+    }
+</style>
