911879c14957feae580762964f5936a2939c1d7c
yuribertoli <yuribertoli@yahoo.it>
2022-02-23 20:06:17 +0100

    main

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,24 +1,84 @@
 <template>
-  <main></main>
+  <main>
+
+    <div class="loading" v-if="caricamento">
+
+        <div class="container">
+
+            <img src="../assets/img/loading.png" alt="Loading">
+
+        </div>
+        
+    </div>
+
+    <div class="container" v-else>
+
+        <div class="albums">
+
+            <MyCard :disco="album" v-for="(album, indice) in albums" :key="indice"/>
+
+        </div>
+        
+    </div>
+
+  </main>
 </template>
 
 <script>
+import MyCard from "./partials/MyCard.vue";
+
+const axios = require('axios');
+
 export default {
+
     name: "MyMain",
+
+    components: {
+        MyCard
+    },
+
     data() {
         return {
-
+            albums: [],
+            caricamento: true,
+            endpoint: 'https://flynn.boolean.careers/exercises/api/array/music'
         }
     },
-    methods: {
 
+    methods: {  
+        pippo() {
+            axios.get(this.endpoint)
+                .then((risposta) => {
+                    console.log(risposta);
+                    this.albums = risposta.data.response;
+                    this.caricamento = false;
+                    console.log(this.albums);
+            })
+        }
     },
-    mounted() {
 
+    mounted() {
+        this.pippo();
     }
 }
 </script>
 
 <style scoped lang="scss">
+.loading {
+    .container {
+        display: flex;
+        justify-content: center;
+
+        img {
+            padding-top: 10%;
+        }
+    }
+}
+
+.albums {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+}
 
 </style>
---
+++ src/components/partials/MyCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="card">
+
+      <img :src="disco.poster" :alt="disco.author + ' - ' + disco.title">
+
+      <h3>{{disco.title}}</h3>
+
+      <span>{{disco.author}}</span>
+
+      <span>{{disco.year}}</span>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+    name: "MyCard",
+
+    props: {
+        "disco": Object
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../assets/style/variables.scss";
+
+.card {
+    width: calc(100% / 5 - 50px);
+    height: 300px;
+    padding: 10px;
+    margin: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: $color1;
+
+    img {
+        width: 100%;
+        height: auto;
+    }
+    h3 {
+        margin: 10px 0;
+        text-transform: uppercase;
+        text-align: center;
+    }
+    span {
+        color: grey;
+        font-size: 0.8rem;
+    }
+}
+
+</style>
