f49904ee840e7598be07c4eecf253fb5873c0d89
yuribertoli <yuribertoli@yahoo.it>
2022-02-23 18:43:57 +0100

    creazione main

---
+++ src/components/MyMain.vue
@@ -0,0 +1,24 @@
+<template>
+  <main></main>
+</template>
+
+<script>
+export default {
+    name: "MyMain",
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
