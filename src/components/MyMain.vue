87ac3a558e91c8d0ff2b71cfcb7aa162639f6ec4
yuribertoli <yuribertoli@yahoo.it>
2022-02-24 15:14:24 +0100

    get genre

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
 
+    <!-- se la variabile caricamento è true, allora mostro l'immagine -->
     <div class="loading" v-if="caricamento">
 
         <div class="container">
@@ -15,6 +16,7 @@
 
         <div class="albums">
 
+            <!-- ciclo l'array per ricavarne ogni oggetto presente, passo poi questi valori al componente figlio MyCard tramite la props :disco -->
             <MyCard :disco="album" v-for="(album, indice) in albums" :key="indice"/>
 
         </div>
@@ -27,7 +29,7 @@
 <script>
 import MyCard from "./partials/MyCard.vue";
 
-const axios = require('axios');
+const axios = require('axios').default;
 
 export default {
 
@@ -47,13 +49,33 @@ export default {
 
     methods: {  
         pippo() {
+
+            //effettuo una chiamata all'url indicato dalla variabile endpoint
             axios.get(this.endpoint)
                 .then((risposta) => {
                     console.log(risposta);
+
+                    //inserisco i dati dell'array nella variabile vuota albums
                     this.albums = risposta.data.response;
+
+                    //imposto caricamento su false per far funzionare il v-if nell'html e non mostrare più il div di caricamento
                     this.caricamento = false;
                     console.log(this.albums);
             })
+        }
+    },
+
+    //utilizzo computed per filtrare dei dati già posseduti in modo da ottimizzare le risorse e non dover effettuare la chiamata al server se i dati di partenza non sono cambiati
+    computed: {
+
+        //creo un array che prende come elementi i generi dell'array albums
+        generi(){
+            let generiMusicali = [];
+            for (let i=0; i<this.albums.length; i++) {
+                generiMusicali.push(this.albums[i].genre);
+            }
+            console.log('i generi musicali sono: ' + generiMusicali);
+            return generiMusicali;
         }
     },
 
